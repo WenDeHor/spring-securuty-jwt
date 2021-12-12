70a17b42c76c0619b6c14db64c24a8ea0c79412e
gorno <xalivan@ukr.net>
2021-12-12 14:40:37 +0200

    1.clean

---
+++ src/test/java/com/example/springsecurutyjwt/SpringSecurutyJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurutyjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurutyJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
